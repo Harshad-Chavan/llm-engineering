1b214ae289cddf120ac1e1f4d14e7395004566fd
harshad <harshadchavan141@gmail.com>
2026-01-01 15:59:19 +0530

    brochure generator basic

---
+++ brochure_generator.py
@@ -0,0 +1,114 @@
+import os
+import json
+from dotenv import load_dotenv
+from IPython.display import Markdown, display, update_display
+from scrapper import fetch_website_links, fetch_website_contents
+from openai import OpenAI
+
+
+def get_links_user_prompt(url):
+    user_prompt = f"""
+Here is the list of links on the website {url} -
+Please decide which of these are relevant web links for a brochure about the company, 
+respond with the full https URL in JSON format.
+Do not include Terms of Service, Privacy, email links.
+
+Links (some might be relative links):
+                    
+"""
+    links = fetch_website_links(url)
+    user_prompt += "\n".join(links)
+    return user_prompt
+
+def select_relevant_links(url):
+    print("Selecting relevant links...")
+
+    response = openai.chat.completions.create(
+        model=MODEL,
+        messages=[
+            {"role": "system", "content": link_system_prompt},
+            {"role": "user", "content": get_links_user_prompt(url)}
+        ],
+        response_format={'type': 'json_object' }
+    )
+    content = response.choices[0].message.content
+    try:
+        data = json.loads(content)
+        print("Relevant links selected: {}".format(len(data)))
+        return data
+    except json.JSONDecodeError:
+        print("Failed to parse JSON response")
+        return []
+
+def fetch_page_and_all_relevant_links(url):
+    contents = fetch_website_contents(url)
+    relevant_links = select_relevant_links(url)
+    result = f"## Landing Page:\n\n{contents}\n## Relevant Links:\n"
+    for link in relevant_links['links']:
+        result += f"\n\n### Link: {link['type']}\n"
+        result += fetch_website_contents(link["url"])
+    return result
+
+# Initialize and constants
+
+load_dotenv(override=True)
+api_key = os.getenv('OPENAI_API_KEY')
+
+if api_key and api_key.startswith('sk-proj-') and len(api_key) > 10:
+    print("API key looks good so far")
+else:
+    print("There might be a problem with your API key? Please visit the troubleshooting notebook!")
+
+MODEL = 'gpt-5-nano'
+openai = OpenAI()
+
+links = fetch_website_links("https://edwarddonner.com")
+
+link_system_prompt = """
+You are provided with a list of links found on a webpage.
+You are able to decide which of the links would be most relevant to include in a brochure about the company,
+such as links to an About page, or a Company page, or Careers/Jobs pages.
+You should respond in JSON as in this example:
+
+{
+    "links": [
+        {"type": "about page", "url": "https://full.url/goes/here/about"},
+        {"type": "careers page", "url": "https://another.full.url/careers"}
+    ]
+}
+"""
+
+# print(get_links_user_prompt("https://edwarddonner.com"))
+# print(select_relevant_links("https://edwarddonner.com"))
+brochure_system_prompt = """
+You are an assistant that analyzes the contents of several relevant pages from a company website
+and creates a short brochure about the company for prospective customers, investors and recruits.
+Respond in markdown without code blocks.
+Include details of company culture, customers and careers/jobs if you have the information.
+"""
+
+def get_brochure_user_prompt(company_name, url):
+    user_prompt = f"""
+You are looking at a company called: {company_name}
+Here are the contents of its landing page and other relevant pages;
+use this information to build a short brochure of the company in markdown without code blocks.\n\n
+"""
+    user_prompt += fetch_page_and_all_relevant_links(url)
+    user_prompt = user_prompt[:5_000] # Truncate if more than 5,000 characters
+    return user_prompt
+
+get_brochure_user_prompt("HuggingFace", "https://huggingface.co")
+
+def create_brochure(company_name, url):
+    response = openai.chat.completions.create(
+        model="gpt-4.1-mini",
+        messages=[
+            {"role": "system", "content": brochure_system_prompt},
+            {"role": "user", "content": get_brochure_user_prompt(company_name, url)}
+        ],
+    )
+    result = response.choices[0].message.content
+    display(Markdown(result))
+
+print(create_brochure("HuggingFace", "https://huggingface.co"))
+
